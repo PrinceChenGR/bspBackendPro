ab171f6290be50465c47f3cec0c729535f54b7dd
muxigame <76636667+muxigame@users.noreply.github.com>
2023-02-16 22:32:48 +0800

    修改url参数至请求体

--- src/main/java/com/example/bsppro/controller/LoginController.java
+++ src/main/java/com/example/bsppro/controller/LoginController.java
@@ -3,12 +3,16 @@ package com.example.bsppro.controller;
 import com.example.bsppro.entity.Artisan;
 import com.example.bsppro.mapper.ArtisanMapper;
 import com.example.bsppro.service.ArtisanService;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.Console;
+import java.util.Map;
 
+@Log4j2
 @CrossOrigin
 @RestController
 public class LoginController {
@@ -16,10 +20,13 @@ public class LoginController {
     @Autowired
     private ArtisanService artisanService;
 
-    @GetMapping("/Login/{data}")
+    @PostMapping("/Login")
     @ResponseBody
-    public String findOne(@PathVariable("data")String data){
-        if(data!=null){
+    public String findOne(@RequestBody Map<String, Object> param){
+        if(param!=null){
+            log.info(param.get("usersname"));
+            log.info(param.get("password"));
+            log.info(param.get("position"));
             System.out.println("传输正常");
             return "ok";
         }else{
